c403857af06c764747bec4b5add3a2672a073da1
minglu_ge <geminglu199698@outlook.com>
2023-04-09 15:26:12 +0800

    feat: typeROM基础配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
-import { Module, MiddlewareConsumer, Logger } from '@nestjs/common';
+import { Module, MiddlewareConsumer, Logger, Dependencies } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
+import dsConfig from 'src/config/dbConfig';
+@Dependencies(DataSource)
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.local', `.env.${process.env.NODE_ENV}`],
+      isGlobal: true,
+      load: [dsConfig],
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => {
+        return {
+          ...(await ConfigService.get('database')),
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
---
+++ src/config/dbConfig.ts
@@ -0,0 +1,16 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT as unknown as number,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['/entities/*.ts'],
+  logger: 'file',
+  logging: true,
+  autoLoadEntities: true,
+  synchronize: true,
+  timezone: 'UTC',
+}));
