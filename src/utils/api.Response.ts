bdd80006dacc5258d71555c0d4b00bfa0c17f041
minglu_ge <geminglu199698@outlook.com>
2023-06-14 23:12:18 +0800

    feat: 响应体分装

---
+++ src/utils/api.Response.ts
@@ -0,0 +1,166 @@
+import { Type, applyDecorators } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  getSchemaPath,
+  ApiInternalServerErrorResponse,
+  ApiCreatedResponse,
+  ApiUnauthorizedResponse,
+  refs,
+} from '@nestjs/swagger';
+import { SchemaObject, ReferenceObject } from '@nestjs/swagger/dist/interfaces/open-api-spec.interface';
+
+const baseTypeNames = ['String', 'Number', 'Boolean'];
+/**
+ * 封装 swagger 返回统一结构
+ * 支持复杂类型 {  status, message, data }
+ * @param status 自定义状态码
+ * @param model 返回的 data 的数据类型
+ * @param isArray data 是否是数组
+ * @param isPager 设置为 true, 则 data 类型为 { list, total, page, pageSize } , false data 类型是纯数组
+ */
+export const SuccessResponse = <TModel extends Type<any>>(
+  status: any,
+  model?: TModel,
+  isArray?: boolean,
+  isPager?: boolean,
+) => {
+  let items: SchemaObject & Partial<ReferenceObject> = null;
+  if (model && baseTypeNames.includes(model.name)) {
+    items = { type: model.name.toLocaleLowerCase() };
+  } else {
+    items = { $ref: getSchemaPath(model) };
+  }
+
+  let prop: SchemaObject & Partial<ReferenceObject> = null;
+  if (isArray && isPager) {
+    prop = {
+      type: 'object',
+      properties: {
+        list: {
+          type: 'array',
+          items,
+        },
+        total: {
+          type: 'number',
+          default: 0,
+        },
+        page: {
+          type: 'number',
+          default: 1,
+        },
+        pageSize: {
+          type: 'number',
+          default: 10,
+        },
+      },
+      required: ['list', 'total', 'page', 'pageSize'],
+    };
+  } else if (isArray) {
+    prop = {
+      type: 'array',
+      items,
+    };
+  } else if (model) {
+    prop = items;
+  } else {
+    prop = { type: 'null', default: null };
+  }
+
+  return applyDecorators(
+    status({
+      schema: {
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              data: prop,
+              message: {
+                type: 'string',
+                default: '',
+              },
+              success: {
+                type: 'boolean',
+                default: true,
+              },
+            },
+            required: ['success'],
+          },
+        ],
+      },
+    }),
+  );
+};
+
+/**
+ * 成功状态的返回结构
+ * @param model 返回的 data 的数据类型
+ * @param isArray data 是否是数组
+ * @param isPager 设置为 true, 则 data 类型为 { list, total, page, pageSize } , false data 类型是纯数组
+ */
+export const ResSuccess = <TModel extends Type<any>>(model?: TModel, isArray?: boolean, isPager?: boolean) => {
+  return SuccessResponse(ApiOkResponse, model, isArray, isPager);
+};
+
+/**
+ * 创建成功的返回结构
+ * @param model 返回的 data 的数据类型
+ * @param isArray data 是否是数组
+ * @param isPager 设置为 true, 则 data 类型为 { list, total, page, pageSize } , false data 类型是纯数组
+ */
+export const ResCerated = <TModel extends Type<any>>(model?: TModel, isArray?: boolean, isPager?: boolean) => {
+  return SuccessResponse(ApiCreatedResponse, model, isArray, isPager);
+};
+
+/**
+ * 服务器出错的返回结构
+ */
+export const ResServerErrorResponse = () => {
+  return applyDecorators(
+    ApiInternalServerErrorResponse({
+      description: '服务器错误',
+      schema: {
+        allOf: [
+          {
+            type: 'object',
+            properties: {
+              message: {
+                type: 'string',
+                default: 'server error',
+              },
+              success: {
+                type: 'boolean',
+                default: false,
+              },
+            },
+            required: ['success', 'message'],
+          },
+        ],
+      },
+    }),
+  );
+};
+
+export const ResUnauthorized = () => {
+  return applyDecorators(
+    ApiUnauthorizedResponse({
+      description: '未认证',
+      schema: {
+        allOf: [
+          {
+            properties: {
+              message: {
+                type: 'string',
+                default: '请先登陆',
+              },
+              success: {
+                type: 'boolean',
+                default: false,
+              },
+            },
+            required: ['success', 'message'],
+          },
+        ],
+      },
+    }),
+  );
+};
---
+++ src/utils/result.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+export class ResultData {
+  constructor(success = true, message?: string, data?: any) {
+    this.message = message || '';
+    this.data = data || null;
+    this.success = success;
+  }
+
+  @ApiProperty({ type: 'string', default: '' })
+  message?: string;
+
+  @ApiProperty({ type: 'any' })
+  data?: any;
+
+  @ApiProperty({ type: 'boolean' })
+  success: boolean;
+
+  /**
+   * 成功
+   * @param data 响应主体
+   * @param message 内容描述
+   * @returns
+   */
+  static ok(data?: any, message?: string): ResultData {
+    return new ResultData(true, message, data);
+  }
+
+  /**
+   *失败
+   * @param message 失败描述
+   * @param data 响应主体
+   * @returns
+   */
+  static fail(message?: string, data?: any): ResultData {
+    return new ResultData(false, message, data);
+  }
+}
