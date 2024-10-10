e6900c754bcc415ab600d1b6abbb36c3af02dd06
minglu_ge <geminglu199698@outlook.com>
2024-10-10 08:40:10 +0800

    feat: TypeOrmModuleOptions

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,16 +1,20 @@
 import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export default registerAs('database', () => ({
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as unknown as number,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: ['/entities/*.ts'],
-  logger: 'file',
-  logging: true,
-  autoLoadEntities: true,
-  synchronize: false,
-  timezone: 'UTC',
-}));
+export default registerAs(
+  'database',
+  (): TypeOrmModuleOptions => ({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT as unknown as number,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: ['/entities/*.ts'],
+    logger: 'file',
+    logging: true,
+    autoLoadEntities: true,
+    synchronize: false,
+    timezone: 'UTC',
+  }),
+);
