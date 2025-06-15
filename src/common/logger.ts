a8bcd6b1b4ff14f7ce11aa80291df8b77422fb89
minglu_ge <geminglu199698@outlook.com>
2025-06-15 11:40:33 +0800

    feat: logger update

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -4,58 +4,28 @@
  */
 import { createLogger, format, transports, Logger } from 'winston';
 
-const { combine, timestamp, printf, prettyPrint, align } = format;
+const { combine, timestamp, printf } = format;
 
 const myFormat = printf(
-  ({ level, message, timestamp, stack, context }) => `[${level}] [${timestamp}] [${context}] ${message} ${stack || ''}`,
+  ({ level, message, timestamp, stack, context }) =>
+    `[${level}] [${timestamp as string}] [${context as string}] ${message as string} ${
+      (stack as string) || ''
+    }`,
 );
 
 const winstonLogger: Logger = createLogger();
 
-// if (process.env.NODE_ENV === 'production') {
 winstonLogger.add(
-  new transports.File({
-    filename: 'logs/info.log',
-    level: 'info',
+  new transports.Console({
+    level: 'silly',
     format: combine(
+      format.colorize(),
       timestamp({
         format: () => new Date().toLocaleString('zh-GB'),
       }),
-      align(),
-      prettyPrint({ colorize: true, depth: 20 }),
       myFormat,
     ),
   }),
 );
-
-winstonLogger.add(
-  new transports.File({
-    filename: 'logs/error.log',
-    level: 'error',
-    format: combine(
-      timestamp({
-        format: () => new Date().toLocaleString('zh-GB'),
-      }),
-      align(),
-      prettyPrint({ colorize: true, depth: 20 }),
-      myFormat,
-    ),
-  }),
-);
-// }
-if (process.env.NODE_ENV === 'development') {
-  winstonLogger.add(
-    new transports.Console({
-      level: 'silly',
-      format: combine(
-        format.colorize(),
-        timestamp({
-          format: () => new Date().toLocaleString('zh-GB'),
-        }),
-        myFormat,
-      ),
-    }),
-  );
-}
 
 export default winstonLogger;
